7c6ab633122572f4ef6fa277aff967feef46913e
Jakub Urban <JakubUrban@email.cz>
2022-01-06 15:49:48 +0100

    Added session

--- header.php
+++ header.php
@@ -6,20 +6,12 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title>Currency Converter</title>
+	<title>Sample shop</title>
 
 	<link rel="stylesheet"
 		href=
 "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<script src=
-"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
-	</script>
-	<script src=
-"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js">
-	</script>
-	<script src=
-"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js">
-	</script>
+
 
 	<link rel="preconnect"
 		href="https://fonts.gstatic.com">
@@ -31,7 +23,5 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-	<!-- Currency Converter -->
-	<h1 class="heading text-center display-2">
+	<h1 class="heading text-center text-white" style="background-color: #2980b9;">
 		Sample shop</h1>
